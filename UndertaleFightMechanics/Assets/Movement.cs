bdfd76044dc09e100f75c5929668a121dd460e19
FireTechStudios <laidlawawz@gmail.com>
2018-11-20 08:05:51 +0000

    InitialCommit

---
+++ UndertaleFightMechanics/Assets/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject player;
+    public float x = 0;
+    public float y = 0;
+
+    public float speed;
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            y = speed;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            y = -speed;
+        else
+            y = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            x = speed;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            x = -speed;
+        else
+            x = 0;
+
+
+        rb.velocity = new Vector2(x, y);
+    }
+}
---
+++ UndertaleFightMechanics/Assets/ParticleCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollider : MonoBehaviour {
+
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public static bool isDead;
+
+    void Start()
+    {
+        isDead = false;
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Player")
+        {
+            isDead = true;
+        }
+    }
+}
