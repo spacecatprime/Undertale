c8d865a5bd38e5aaccc5a46001921b9595fd387a
FireTechStudios <laidlawawz@gmail.com>
2018-11-21 07:58:36 +0000

    BetterProjectStructure

---
+++ UndertaleEndless/Assets/DevWindowResize.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevWindowResize : MonoBehaviour {
+
+    int lastWidth = Screen.width;
+    int lastHeight = Screen.height;
+    bool isReseting = false;
+
+    void LateUpdate()
+    {
+        if (Camera.main.aspect != 0.375f && !isReseting)
+        {
+            if (Screen.width != lastWidth || Screen.height != lastHeight)
+            {
+                // user is resizing width
+                StartCoroutine(SetResolution());
+                lastWidth = Screen.width;
+                lastHeight = Screen.height;
+            }
+        }
+    }
+    IEnumerator SetResolution()
+    {
+        isReseting = true;
+        Screen.fullScreen = !Screen.fullScreen;
+        Screen.SetResolution(Screen.height * 9/16, Screen.height, false);
+        yield return new WaitForSeconds(0.5F);
+        isReseting = false;
+    }
+}
---
+++ UndertaleEndless/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isDead;
+    public float bulletAmount = 0;
+    public TextMeshProUGUI Score;
+    public float score;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        score += 1.0f * Time.deltaTime;
+
+
+        if(isDead == true)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        Score.text = ("Score: " + Mathf.RoundToInt(score).ToString());
+    }
+
+
+}
