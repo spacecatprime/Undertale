9204c81cc227574dee56f0c6fdb8d1625c48d4b6
FireTechStudios <laidlawawz@gmail.com>
2018-12-30 18:49:39 +0000

    Test Monster fade complete. Before full implementation

---
+++ UndertaleEndless/Assets/DustBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpriteShatter;
+
+public class DustBehaviour : MonoBehaviour {
+
+    public bool shouldFadeOut;
+    public bool shouldMove;
+    public float distanceFromTop;
+    public SpriteRenderer sprite;
+
+    // Use this for initialization
+    void Start () {
+        distanceFromTop = new Vector2(this.transform.localPosition.y, this.transform.position.y).magnitude;
+        distanceFromTop = Mathf.Abs(3 - distanceFromTop)/1.2f;
+
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        sprite = this.GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(shouldFadeOut)
+        {
+            sprite.color = new Color(1, 1, 1, sprite.color.a - 2f * Time.deltaTime);
+        }
+
+        if(sprite.color.a <= 0.05f)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        WaitParticle();
+    }
+
+    public void WaitParticle()
+    {
+        if (distanceFromTop < 0 && !shouldFadeOut)
+        {
+            shouldFadeOut = true;
+            var x = UnityEngine.Random.Range(-0.5f, 0.5f);
+            var y = 1f;
+            var direction = new Vector2(x, y);
+            //if you need the vector to have a specific length:
+            direction = direction.normalized * 5f;
+
+            this.GetComponent<Rigidbody2D>().AddForce(direction * 20f);
+        }
+        else
+        {
+            distanceFromTop -= Time.deltaTime;
+        }
+    }
+}
---
+++ UndertaleEndless/Assets/Sprite Shatter/Example Scene/Circus.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using SpriteShatter;
+
+public class Circus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//If the user clicks the left mouse button, explode the circus!
+		if (Input.GetMouseButtonDown(0))
+			GetComponent<Shatter>().shatter();
+
+		//If the user clicks the right mouse button, reset the circus!
+		else if (Input.GetMouseButtonDown(1))
+			GetComponent<Shatter>().reset();
+	}
+}
